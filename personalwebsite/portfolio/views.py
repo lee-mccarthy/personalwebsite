dcb00047ff0d2715fb4eac51556292a182b56521
Lee McCarthy <lee@leemccarthy.com>
2021-03-25 16:15:01 -0400

    Sorted projects by pk descending

--- personalwebsite/portfolio/views.py
+++ personalwebsite/portfolio/views.py
@@ -17,7 +17,7 @@ class Index(FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['projects'] = Project.objects.all()
+        context['projects'] = Project.objects.order_by('-pk')
         return context
 
 
