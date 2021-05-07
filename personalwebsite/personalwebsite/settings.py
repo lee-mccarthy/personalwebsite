7c3f2a0649bb04670563e5354e4e034ed58069e9
Lee McCarthy <lee@leemccarthy.com>
2021-05-07 14:32:39 -0400

    Added DEFAULT_AUTO_FIELD in settings.py

--- personalwebsite/personalwebsite/settings.py
+++ personalwebsite/personalwebsite/settings.py
@@ -148,6 +148,12 @@ if not DEBUG:
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
 # Media files
 
 MEDIA_URL = '/media/'
